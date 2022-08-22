fbbeffaf5347896b824db3adabbbfd8e996c3e4a
Afonso-cmd <afonso.ur@gmail.com>
2022-08-22 08:24:10 -0300

    operations

--- build/index.js
+++ build/index.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log("Test");
+const input1 = document.getElementById('num1');
+const input2 = document.getElementById('num2');
+const btnSum = document.getElementById('btnSum');
+const btnSub = document.getElementById('btnSub');
+function sum(a, b) {
+    return a + b;
+}
+function sub(a, b) {
+    return a - b;
+}
+btnSum.addEventListener('click', function () {
+    const result = sum(Number(input1.value), Number(input2.value));
+    console.log(result);
+});
+btnSub.addEventListener('click', function () {
+    const result = sub(Number(input1.value), Number(input2.value));
+    console.log(result);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Test")
+const input1 = document.getElementById('num1') as HTMLInputElement;
+const input2 = document.getElementById('num2') as HTMLInputElement;
+
+const btnSum =document.getElementById('btnSum') as HTMLElement;
+const btnSub =document.getElementById('btnSub') as HTMLElement;
+
+function sum (a:number, b:number){
+    return a + b;
+}
+
+function sub(a:number, b:number){
+    return a - b;
+}
+
+btnSum.addEventListener('click', function(){
+    const result = sum(Number(input1.value),Number(input2.value));
+    console.log(result) 
+})
+
+btnSub.addEventListener('click', function(){
+    const result = sub(Number(input1.value),Number(input2.value));
+    console.log(result) 
+})
